d68f0eb74376a83ff191265bacd201805f47086e
DangHoaiPhong <DangHoaiPhong@192.168.1.3>
2020-04-08 15:38:32 +0700

    Ngay 8/4 hoan thanh BT1

---
+++ src/baitap1/BaiTap1Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baitap1;
+/**
+ *
+ * @author DangHoaiPhong
+ */
+public class BaiTap1Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+           
+        NhanVien nv1=new NhanVien("Dang Hoai Phong", 21, "Ninh Hoa",10000000,200);
+        NhanVien nv2=new NhanVien("DHP", 22, "Nha Trang",20000000,100);
+    }
+    
+}
